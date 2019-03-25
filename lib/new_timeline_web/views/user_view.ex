0dfb8a5777a200b4c87869776d1ce7892adaae0c
Rachael Serur <rachaelse@gmail.com>
2019-03-24 21:00:25 -0400

    Add user JSON endpoints, update tests

---
+++ lib/new_timeline_web/views/user_view.ex
@@ -0,0 +1,18 @@
+defmodule NewTimelineWeb.UserView do
+  use NewTimelineWeb, :view
+  alias NewTimelineWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      email: user.email,
+      is_active: user.is_active}
+  end
+end
--- notes.md
+++ notes.md
@@ -3,12 +3,17 @@
 $ mix phx.new new-timeline --app new_timeline --module NewTimeline --no-html --no-webpack
 
 $ mix phx.gen.context Auth User users email:string:unique is_active:boolean
+
+mix phx.gen.json Auth User users email:string password:string \
+is_active:boolean --no-context --no-schema
 ```
 
 # Common Commands
 ```
 $ mix phx.server
 $ mix ecto.create
+$ mix ecto.migrate
 $ mix ecto.drop
 $ MIX_ENV=test mix ecto.drop
+$ iex -S mix phx.server
 ```
